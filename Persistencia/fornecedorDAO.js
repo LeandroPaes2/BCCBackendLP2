e37dbd717301dc7c2ac714d8d6338fd8809a3a43
LeandroPaes2 <lelepaes205@gmail.com>
2024-12-06 03:05:05 -0300

    forn

--- Persistencia/fornecedorDAO.js
+++ Persistencia/fornecedorDAO.js
@@ -1,103 +1,110 @@
-// DAO - Data Access Object
 import Fornecedor from "../Modelo/fornecedor.js";
 import conectar from "./Conexao.js";
 
 export default class FornecedorDAO {
+
     constructor() {
         this.init();
     }
 
     async init() {
         try {
-            const conexao = await conectar(); // Retorna uma conexão
+            const conexao = await conectar();
             const sql = `
-            CREATE TABLE IF NOT EXISTS fornecedor (
-                forn_codigo INT NOT NULL AUTO_INCREMENT,
-                forn_nome VARCHAR(200) NOT NULL,
-                forn_endereco VARCHAR(255) NOT NULL,
-                forn_contato VARCHAR(50) NOT NULL,
-                forn_cpf VARCHAR(15) NOT NULL,
-                CONSTRAINT pk_fornecedor PRIMARY KEY (forn_codigo)
-            )
+                CREATE TABLE IF NOT EXISTS fornecedor(
+                    codigo INT NOT NULL AUTO_INCREMENT,
+                    nome VARCHAR(100) NOT NULL,
+                    endereco VARCHAR(255) NOT NULL,
+                    contato VARCHAR(50) NOT NULL,
+                    cpf VARCHAR(15) NOT NULL,
+                    CONSTRAINT pk_fornecedor PRIMARY KEY (codigo)
+                );
             `;
             await conexao.execute(sql);
             await conexao.release();
-        } catch (e) {
-            console.log("Não foi possível iniciar o banco de dados: " + e.message);
+        } catch (erro) {
+            console.log("Erro ao iniciar a tabela fornecedor: " + erro.message);
         }
     }
 
-    async incluir(fornecedor) {
+    async gravar(fornecedor) {
         if (fornecedor instanceof Fornecedor) {
             const conexao = await conectar();
             const sql = `
-                INSERT INTO fornecedor (forn_nome, forn_endereco, forn_contato, forn_cpf)
+                INSERT INTO fornecedor (nome, endereco, contato, cpf) 
                 VALUES (?, ?, ?, ?)
             `;
             const parametros = [
                 fornecedor.nome,
                 fornecedor.endereco,
                 fornecedor.contato,
-                fornecedor.cpf,
+                fornecedor.cpf
             ];
             const resultado = await conexao.execute(sql, parametros);
-            fornecedor.codigo = resultado[0].insertId; // Atualiza o código do fornecedor
-            await conexao.release(); // Libera a conexão
+            fornecedor.codigo = resultado[0].insertId;
+            await conexao.release();
         }
     }
 
-    async alterar(fornecedor) {
+    async editar(fornecedor) {
         if (fornecedor instanceof Fornecedor) {
             const conexao = await conectar();
             const sql = `
                 UPDATE fornecedor 
-                SET forn_nome = ?, forn_endereco = ?, forn_contato = ?, forn_cpf = ?
-                WHERE forn_codigo = ?
+                SET nome = ?, endereco = ?, contato = ?, cpf = ? 
+                WHERE codigo = ?
             `;
             const parametros = [
                 fornecedor.nome,
                 fornecedor.endereco,
                 fornecedor.contato,
                 fornecedor.cpf,
-                fornecedor.codigo,
+                fornecedor.codigo
             ];
             await conexao.execute(sql, parametros);
-            await conexao.release(); // Libera a conexão
+            await conexao.release();
         }
-    }
-
-    async consultar(termo) {
-        const conexao = await conectar();
-        let sql = "";
-        let parametros = [];
-
-        if (isNaN(parseInt(termo))) {
-            sql = `SELECT * FROM fornecedor WHERE forn_nome LIKE ?`;
-            parametros = ['%' + termo + '%'];
-        } else {
-            sql = `SELECT * FROM fornecedor WHERE forn_codigo = ?`;
-            parametros = [termo];
-        }
-
-        const [linhas] = await conexao.execute(sql, parametros);
-        const listaFornecedores = linhas.map(linha => new Fornecedor(
-            linha["forn_codigo"],
-            linha["forn_nome"],
-            linha["forn_endereco"],
-            linha["forn_contato"],
-            linha["forn_cpf"]
-        ));
-        await conexao.release();
-        return listaFornecedores;
     }
 
     async excluir(fornecedor) {
         if (fornecedor instanceof Fornecedor) {
             const conexao = await conectar();
-            const sql = `DELETE FROM fornecedor WHERE forn_codigo = ?`;
+            const sql = "DELETE FROM fornecedor WHERE codigo = ?";
             const parametros = [fornecedor.codigo];
             await conexao.execute(sql, parametros);
-            await conexao.release(); // Libera a conexão
+            await conexao.release();
         }
+    }
+
+    async consultar(termo) {
+        let sql = "";
+        let parametros = [];
+        if (isNaN(parseInt(termo))) {
+            sql = `
+                SELECT * FROM fornecedor 
+                WHERE nome LIKE ? OR endereco LIKE ? OR contato LIKE ? 
+                ORDER BY nome
+            `;
+            parametros = Array(3).fill(`%${termo}%`);
+        } else {
+            sql = "SELECT * FROM fornecedor WHERE codigo = ? ORDER BY nome";
+            parametros = [termo];
+        }
+
+        const conexao = await conectar();
+        const [registros, campos] = await conexao.query(sql, parametros);
+        await conexao.release();
+
+        const listaFornecedores = registros.map(registro => 
+            new Fornecedor(
+                registro['codigo'],
+                registro['nome'],
+                registro['endereco'],
+                registro['contato'],
+                registro['cpf']
+            )
+        );
+
+        return listaFornecedores;
     }
 }
